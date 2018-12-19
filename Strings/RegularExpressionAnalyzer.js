48587bf25f317403a16b7503aa716ad0623549df
ahouck <ahouck@users.noreply.github.com>
2018-12-19 15:51:39 -0500

    Create RegularExpressionAnalyzer: Interview question

---
+++ Strings/RegularExpressionAnalyzer.js
@@ -0,0 +1,10 @@
+/**
+ * Function question_wildcard_match(text, query) adds the quantifier metacharacter ? to the query character set. x? 
+ * in query matches either zero occurrences or one occurrence of x in text, where x is a non-quantifier character.
+ *
+ */
+ 
+ 
+ function question_wildcard_match(text, query) {
+    
+ {
